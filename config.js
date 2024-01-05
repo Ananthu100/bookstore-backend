91a7b7e6e84c9cca37e5022d054dc6f06429e135
Ananthanarayanan <erananthu100@gmail.com>
2024-01-05 15:36:30 +0530

    Updation

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
-export const mongoDBURL =
-  "mongodb+srv://root:fU7NNJbEkAG5vV3@books-store-mern.7v87dxl.mongodb.net/books-collection?retryWrites=true&w=majority";
+import dotenv from "dotenv";
+dotenv.config();
+export const mongoDBURL = process.env.connection_string;
